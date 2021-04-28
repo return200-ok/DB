a44978b0b005be849f9a81f77118453d4035d585
Le Van Cao <caolv@eway.vn>
2021-04-28 10:02:51 +0700

    init

---
+++ setup-mongo.sh
@@ -0,0 +1,5 @@
+#setup mongo
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt update
+sudo apt install mongodb-org
---
+++ sync-restore-pinggo.sh
@@ -0,0 +1,58 @@
+#!/bin/bash 
+################################################################
+##
+##   MySQL Database Backup Script
+##   Last Update: Jul 09, 2020
+##
+################################################################
+ 
+export PATH=/bin:/usr/bin:/usr/local/bin
+#TODAY=`date +'%m-%d-%Y'`
+ 
+DATE=`date +%m%d`
+################################################################
+################## Update below values  ########################
+ 
+DB_BACKUP_PATH='/data/backup/mysql/pinggo-service01'
+HOST="pinggo-service01"
+DATABASE_NAME="pinggo"
+CMD="mysql --defaults-file=/etc/mysql/debian.cnf"
+BACKUP_RETAIN_DAYS=14   ## Number of days to keep local backup copy
+LOGFILE='/root/mysql-backup-output.log'
+#CREDENTIALS_FILE='/home/tuyendd/scripts/extra.my.cnf'
+USERID="-331965421"
+TOKEN="1157459635:AAHoKBShKDnF1UQop23JK4oKapglsjt1mYY"
+TIMEOUT="10"
+URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+DATE_EXEC="$(date "+%d %b %Y %H:%M")"
+TMPFILE='/tmp/ipinfo-$DATE_EXEC.txt'
+#################################################################
+ 
+mkdir -p ${DB_BACKUP_PATH}/${HOST}_db_${DATE}
+echo "${DATE} Backup started for database - ${DATABASE_NAME}" >> $LOGFILE 2>&1
+ 
+ #for DB in $DATABASE_NAME; do
+   #$CMD $DB > ${DB_BACKUP_PATH}/${TODAY}/${DB}-${TODAY}.sql
+rsync -a root@10.130.66.203::mysql/${HOST}/${HOST}_db_${DATE}/${DATABASE_NAME}.${DATE}*.sql.gz ${DB_BACKUP_PATH}/${HOST}_db_${DATE} && \
+gunzip < ${DB_BACKUP_PATH}/${HOST}_db_${DATE}/${DATABASE_NAME}.${DATE}*.sql.gz | $CMD $DATABASE_NAME
+#gzip ${DB_BACKUP_PATH}/${TODAY}/${DB}-${TODAY}.sql
+#done
+
+if [ $? -eq 0 ]; then
+  TEXT=$(echo "${DATE_EXEC} Database $DATABASE_NAME backup successfully completed on adflex-dev-501") && curl -s -X POST --max-time $TIMEOUT $URL -d "chat_id=$USERID" -d text="$TEXT" > /dev/null
+else
+  TEXT1=$(echo "${DATE_EXEC} Error found during backup on adflex-dev-501") && curl -s -X POST --max-time $TIMEOUT $URL -d "chat_id=$USERID" -d text="$TEXT1" > /dev/null
+  exit 1
+fi
+ 
+##### Remove backups older than {BACKUP_RETAIN_DAYS} days  #####
+ 
+DBDELDATE=`date +'%m-%d-%Y' --date="${BACKUP_RETAIN_DAYS} days ago"`
+ 
+if [ ! -z ${DB_BACKUP_PATH} ]; then
+      cd ${DB_BACKUP_PATH}
+      if [ ! -z ${DBDELDATE} ] && [ -d ${DBDELDATE} ]; then
+            rm -rf ${DBDELDATE}
+      fi
+fi
+### End of script ####
