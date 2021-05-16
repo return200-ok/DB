7fd33564a063b1eca4409385aaeda133e087ec0c
levancao798 <53284451+levancao798@users.noreply.github.com>
2021-05-16 22:59:03 +0700

    Delete setup-mongo.sh

--- setup-mongo.sh
+++
@@ -1,5 +0,0 @@
-#setup mongo
-curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-sudo apt update
-sudo apt install mongodb-org
